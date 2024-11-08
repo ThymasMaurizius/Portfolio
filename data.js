80a559df0f64db2f34deb81c1f5e31065b49f0d8
ThymasMaurizius <mayr.thomas1999@gmail.com>
2024-11-08 15:30:17 +0100

    rework

--- data.js
+++ data.js
@@ -1,7 +1,5 @@
 export const bio = [
-  "Hello! I'm Vinay Somawat, an experienced Frontend Software Engineer with a proven ability to upgrade applications, optimize performance, and deliver innovative solutions.",
-  "I am passionate about using my technical skills to drive impactful contributions in challenging projects.",
-  "Thanks for visiting!",
+  "Kreativer Softwareentwickler mit einer Leidenschaft für das Lösen von komplexen Problemen. Ich freue mich darauf, innovative Softwarelösungen zu entwickeln und meine Fähigkeiten in einem Team weiter auszubauen.",
 ];
 
 export const skills = [
